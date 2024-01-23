c5fd047fad13857cd7e8742abae368189c7931a8
Ritwik Srivastava <ritwik@ritwiks-mbp-2.corp.adobe.com>
2024-01-23 15:38:08 +0530

    Add table variants from sunstar

--- blocks/table/table.js
+++ blocks/table/table.js
@@ -1,5 +1,3 @@
-import { readBlockConfig } from '../../scripts/lib-franklin.js';
-
 function buildMutipleTables(block) {
   const mainContent = block.parentNode.parentNode;
   if (mainContent.querySelectorAll(':scope > .flat-table').length < 2) return;
@@ -34,46 +32,59 @@ function buildMutipleTables(block) {
   mainContent.replaceChildren(...mainTmp.childNodes);
 }
 
-export default async function decorate(block) {
-  const blockConfig = readBlockConfig(block);
-  const missingCfgs = [blockConfig.title, blockConfig.annotation].filter((x) => !x).length;
-  const tableDiv = document.createElement('div');
-  tableDiv.classList.add('table-item');
-  const tableAnnotation = document.createElement('p');
-  tableAnnotation.classList.add('table-annotation');
-  const tableTitle = document.createElement('h3');
-  tableTitle.classList.add('table-title');
-
-  if (blockConfig.title) {
-    tableTitle.innerHTML = blockConfig.title;
-  }
-
-  if (blockConfig.annotation) {
-    tableAnnotation.innerHTML = blockConfig.annotation;
-  }
-
-  const tableChild = [...block.children].filter((x) => x.innerText.trim().toLowerCase().startsWith('table'))[0];
-  if (tableDiv.getAttribute('style') === null) {
-    const rowLength = ([...block.children].length - 2 + missingCfgs).toString();
-    const colLength = (tableChild.children.length - 1).toString();
-    tableDiv.setAttribute('style', `--row:${rowLength};--col:${colLength}`);
-  }
-
-  [...tableChild.children].forEach((childDiv, x) => {
-    if (x) {
-      childDiv.classList.add('table-head');
-      tableDiv.append(childDiv);
+function innerTableHighlightFirstColumn(block, noHead) {
+  const trs = block.querySelectorAll('table tr');
+  const tdNumbArray = [];
+  [...trs].forEach((td) => { tdNumbArray.push(td.children.length); });
+  const tdNumb = Math.max(...tdNumbArray);
+  [...trs].forEach((td, index) => {
+    if (noHead && tdNumb === td.children.length) {
+      td.classList.add('highlight-item');
+    } else if (!noHead && index !== 0 && tdNumb === td.children.length) {
+      td.classList.add('highlight-item');
     }
   });
+}
 
-  if (!blockConfig.title) {
+export default async function decorate(block) {
+  const noHead = block.classList.contains('no-head');
+  const tableDiv = document.createElement('div');
+  tableDiv.classList.add('table-item');
+  const tableTitle = document.createElement('h3');
+  tableTitle.classList.add('table-title');
+  const tableAnnotation = document.createElement('p');
+  tableAnnotation.classList.add('table-annotation');
+  [...block.children].forEach((child, i) => {
+    if (i === 0) {
+      tableTitle.innerHTML = [...child.children][1].innerHTML;
+    } else if (i === 1) {
+      tableAnnotation.innerHTML = [...child.children][1].innerHTML;
+    } else {
+      if (tableDiv.getAttribute('style') === null) {
+        const rowLength = ([...block.children].length - 2).toString();
+        const colLength = ([...child.children].length - 1).toString();
+        tableDiv.setAttribute('style', `--row:${rowLength};--col:${colLength}`);
+      }
+      [...child.children].forEach((childDiv, x) => {
+        if (x) {
+          if (i === 2 && !noHead) childDiv.classList.add('table-head');
+          tableDiv.append(childDiv);
+        }
+      });
+    }
+  });
+  if (tableTitle.innerHTML === '') {
     block.replaceChildren(tableDiv);
+    if (tableAnnotation.innerHTML !== '') block.append(tableAnnotation);
   } else {
     block.replaceChildren(tableTitle);
     block.append(tableDiv);
+    if (tableAnnotation.innerHTML !== '') block.append(tableAnnotation);
   }
-
-  if (blockConfig.annotation) block.append(tableAnnotation);
   if (block.classList.contains('flat')) block.parentElement.classList.add('flat-table');
   buildMutipleTables(block);
+  if (block.classList.contains('inner-table')
+      && block.classList.contains('highlight-first-column')) {
+    innerTableHighlightFirstColumn(block, noHead);
+  }
 }
