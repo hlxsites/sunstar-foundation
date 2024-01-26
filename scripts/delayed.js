1101d6cee708df8a946f6ecb016cd517c53b38e5
JiangLong2019 <57093988+JiangLong2019@users.noreply.github.com>
2024-01-26 18:32:52 +0900

    Update

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -40,9 +40,31 @@ async function loadGTM(gaId) {
         j.src =
             'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
         f.parentNode.insertBefore(j, f);
-        }(window, document, 'script', 'dataLayer', ${gaId}));
+        }(window, document, 'script', 'dataLayer', '${gaId}'));
     `;
   document.head.prepend(scriptTag);
+}
+
+function loadGAScript(url, callback) {
+  const head = document.querySelector('head');
+  let script = head.querySelector(`script[src="${url}"]`);
+  if (!script) {
+    script = document.createElement('script');
+    script.src = url;
+    script.async = true;
+    head.append(script);
+    script.onload = callback;
+    return script;
+  }
+  return script;
+}
+
+async function loadGA() {
+  const gaId = 'UA-150288508-1';
+  loadGAScript(`https://www.googletagmanager.com/gtag/js?id=${gaId}`, () => {
+  // eslint-disable-next-line
+    window.dataLayer = window.dataLayer || []; function gtag() { dataLayer.push(arguments); } gtag('js', new Date()); gtag('config', gaId); ga('send', 'pageview'); ga('create', gaId, 'auto');
+  });
 }
 
 if (!isInternalPage()) {
